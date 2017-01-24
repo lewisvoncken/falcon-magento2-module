a37733fb1fcc984c3290d98f76d5b31ad8373f05
Slawomir Iwanczuk <slawomir.iwanczuk@hatimeria.pl>
2017-01-24 12:08:05 +0100

    RG-479 Added product attribute list

---
+++ src/Model/Cart/Item/AttributeList.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Hatimeria\Reagento\Model\Cart\Item;
+
+class AttributeList
+{
+    /**
+     * @var string[]
+     */
+    protected $attributes;
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+}
