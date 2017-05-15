6afec87ec8e02b7864c8cfeccc536f3b92eae5e5
Władysław Brodsky <wladyslaw.brodsky@hatimeria.pl>
2017-05-15 16:40:45 +0200

    Added code to handle 3ds with adyen - expanded array of data is returned to enable redirect to 3rd party for transaction verification

--- src/Model/Plugin/AfterPlaceOrder.php
+++ src/Model/Plugin/AfterPlaceOrder.php
@@ -3,6 +3,7 @@
 namespace Hatimeria\Reagento\Model\Plugin;
 
 use Magento\Sales\Model\Order;
+use Adyen\Payment\Block\Redirect\Validate3d;
 use Magento\Sales\Api\OrderRepositoryInterface;
 
 class AfterPlaceOrder
@@ -16,20 +17,36 @@ class AfterPlaceOrder
      * AfterPlaceOrder constructor.
      * @param OrderRepositoryInterface $orderRepository
      */
-    public function __construct(OrderRepositoryInterface $orderRepository)
-    {
-        $this->orderRepository = $orderRepository;
+    public function __construct(
+        OrderRepositoryInterface $orderRepository
+    ) {
+        $this->orderRepository      = $orderRepository;
     }
 
     /**
      * @param $subject
      * @param $orderId
-     * @return int
+     * @return int|\StdObject
      */
     public function afterPlaceOrder($subject, $orderId)
     {
         /** @var Order $order */
         $order = $this->orderRepository->get($orderId);
+        $payment = $order->getPayment();
+        if ($payment->getAdditionalInformation()) {
+            $paymentAdditionalInfo = $payment->getAdditionalInformation();
+            if ( isset($paymentAdditionalInfo['3dActive']) && true === $paymentAdditionalInfo['3dActive'] ) {
+                $result = [
+                    'issuerUrl' => $paymentAdditionalInfo['issuerUrl'],
+//                    'termUrl'  => $this->adyenValidate3dBlock->getTermUrl(),
+                    'md' => $paymentAdditionalInfo['md'],
+                    'paRequest' => $paymentAdditionalInfo['paRequest']
+                ];
+
+                return $result;
+            }
+        }
+
         return $order->getData('increment_id');
     }
 }
