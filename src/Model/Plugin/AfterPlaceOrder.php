94aec408423c901fe6af1d86cc714a5faeab8f15
Władysław Brodsky <wladyslaw.brodsky@hatimeria.pl>
2017-05-15 17:22:57 +0200

    Attempt to force keys to be shown within response after creating order with ds payment via Adyen Payment

--- src/Model/Plugin/AfterPlaceOrder.php
+++ src/Model/Plugin/AfterPlaceOrder.php
@@ -2,6 +2,7 @@
 
 namespace Hatimeria\Reagento\Model\Plugin;
 
+use Hatimeria\Reagento\Api\UrlInterface;
 use Magento\Sales\Model\Order;
 use Adyen\Payment\Block\Redirect\Validate3d;
 use Magento\Sales\Api\OrderRepositoryInterface;
@@ -13,20 +14,27 @@ class AfterPlaceOrder
      */
     protected $orderRepository;
 
+//    /**
+//     * @var UrlInterface
+//     */
+//    protected $urlBuilder;
+
     /**
      * AfterPlaceOrder constructor.
      * @param OrderRepositoryInterface $orderRepository
      */
     public function __construct(
-        OrderRepositoryInterface $orderRepository
+        OrderRepositoryInterface $orderRepository//,
+        //\Magento\Framework\UrlInterface $urlBuilder
     ) {
         $this->orderRepository      = $orderRepository;
+//        $this->urlBuilder = $urlBuilder;
     }
 
     /**
      * @param $subject
      * @param $orderId
-     * @return mixed
+     * @return int|array
      */
     public function afterPlaceOrder($subject, $orderId)
     {
@@ -36,20 +44,25 @@ class AfterPlaceOrder
         if ($payment->getAdditionalInformation()) {
             $paymentAdditionalInfo = $payment->getAdditionalInformation();
             if ( isset($paymentAdditionalInfo['3dActive']) && true === $paymentAdditionalInfo['3dActive'] ) {
-                $result = new \StdClass();
-                $result->issuerUrl = $paymentAdditionalInfo['issuerUrl'];
-                $result->md        = $paymentAdditionalInfo['md'];
-                $result->paRequest = $paymentAdditionalInfo['paRequest'];
-//                    'issuerUrl' => $paymentAdditionalInfo['issuerUrl'],
-//                    'termUrl'  => $this->adyenValidate3dBlock->getTermUrl(),
-//                    'md' => $paymentAdditionalInfo['md'],
-//                    'paRequest' => $paymentAdditionalInfo['paRequest']
-//                ];
+                $result = [
+                    'issuerUrl' => $paymentAdditionalInfo['issuerUrl'],
+//                    'termUrl'  => $this->_getTermUrl(),
+                    'md' => $paymentAdditionalInfo['md'],
+                    'paRequest' => $paymentAdditionalInfo['paRequest']
+                ];
 
-                return $result;
+                return json_encode($result);
             }
         }
 
         return $order->getData('increment_id');
     }
+
+//    /**
+//     * @return \Hatimeria\Reagento\Api\Data\UrlDataInterface
+//     */
+//    protected function _getTermUrl()
+//    {
+//        return $this->urlBuilder->getUrl('adyen/process/validate3d');
+//    }
 }
