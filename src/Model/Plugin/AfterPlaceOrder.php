e08068e6d9aa6502b2d76a1b901416fd48213f95
Władysław Brodsky <wladyslaw.brodsky@hatimeria.pl>
2017-05-23 16:02:03 +0200

    Added /order/hatimeria route for supporting REST entity as a response for placing an order Added infrastructure required for providing additional data for frontend redirect

--- src/Model/Plugin/AfterPlaceOrder.php
+++ src/Model/Plugin/AfterPlaceOrder.php
@@ -3,6 +3,8 @@
 namespace Hatimeria\Reagento\Model\Plugin;
 
 use Hatimeria\Reagento\Api\UrlInterface;
+use Hatimeria\Reagento\Model\Api\AdyenRedirect;
+use Hatimeria\Reagento\Model\Api\OrderResponse;
 use Magento\Sales\Model\Order;
 use Adyen\Payment\Block\Redirect\Validate3d;
 use Magento\Sales\Api\OrderRepositoryInterface;
@@ -14,55 +16,56 @@ class AfterPlaceOrder
      */
     protected $orderRepository;
 
-//    /**
-//     * @var UrlInterface
-//     */
-//    protected $urlBuilder;
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
 
     /**
      * AfterPlaceOrder constructor.
      * @param OrderRepositoryInterface $orderRepository
      */
     public function __construct(
-        OrderRepositoryInterface $orderRepository//,
-        //\Magento\Framework\UrlInterface $urlBuilder
+        OrderRepositoryInterface $orderRepository,
+        \Magento\Framework\UrlInterface $urlBuilder
     ) {
         $this->orderRepository      = $orderRepository;
-//        $this->urlBuilder = $urlBuilder;
+        $this->urlBuilder = $urlBuilder;
     }
 
     /**
      * @param $subject
      * @param $orderId
-     * @return int|array
+     * @return OrderResponse | string
      */
     public function afterPlaceOrder($subject, $orderId)
     {
         /** @var Order $order */
         $order = $this->orderRepository->get($orderId);
         $payment = $order->getPayment();
+        $obj = new OrderResponse();
         if ($payment->getAdditionalInformation()) {
             $paymentAdditionalInfo = $payment->getAdditionalInformation();
-            if ( isset($paymentAdditionalInfo['3dActive']) && true === $paymentAdditionalInfo['3dActive'] ) {
-                $result = [
-                    'issuerUrl' => $paymentAdditionalInfo['issuerUrl'],
-//                    'termUrl'  => $this->_getTermUrl(),
-                    'md' => $paymentAdditionalInfo['md'],
-                    'paRequest' => $paymentAdditionalInfo['paRequest']
-                ];
+            if ($payment->getMethod() == 'adyen_cc' && isset($paymentAdditionalInfo['3dActive']) && true === $paymentAdditionalInfo['3dActive'] ) {
+                $adyen = new AdyenRedirect();
+                $adyen->setIssuerUrl($paymentAdditionalInfo['issuerUrl']);
+                $adyen->setMd($paymentAdditionalInfo['md']);
+                $adyen->setPaRequest($paymentAdditionalInfo['paRequest']);
+                $adyen->setTermUrl($this->_getTermUrl());
 
-                return json_encode($result);
+                $obj->setAdyen($adyen);
             }
         }
+        $obj->setOrderId($order->getData('increment_id'));
 
-        return $order->getData('increment_id');
+        return $obj;
     }
 
-//    /**
-//     * @return \Hatimeria\Reagento\Api\Data\UrlDataInterface
-//     */
-//    protected function _getTermUrl()
-//    {
-//        return $this->urlBuilder->getUrl('adyen/process/validate3d');
-//    }
+    /**
+     * @return \Hatimeria\Reagento\Api\Data\UrlDataInterface
+     */
+    protected function _getTermUrl()
+    {
+        return $this->urlBuilder->getUrl('adyen/process/validate3d');
+    }
 }
