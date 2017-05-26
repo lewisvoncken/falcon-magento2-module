b49eefec6fed24cbb9c6f4bfbed25eaa52ae3343
Władysław Brodsky <wladyslaw.brodsky@hatimeria.pl>
2017-05-26 13:00:53 +0200

    Modified way of getting order due to absent checkout session

---
+++ src/Controller/Adyen/Validate3d.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hatimeria\Reagento\Controller\Adyen;
+
+class Validate3d extends \Adyen\Payment\Controller\Process\Validate3d
+{
+    /**
+     * Get order object
+     *
+     * @return \Magento\Sales\Model\Order
+     */
+    protected function _getOrder()
+    {
+        if (!$this->_order) {
+            $incrementId = $this->getRequest()->getPost('order_id');
+            $this->_orderFactory = $this->_objectManager->get('Magento\Sales\Model\OrderFactory');
+            $this->_order = $this->_orderFactory->create()->loadByIncrementId($incrementId);
+        }
+        return $this->_order;
+    }
+}
