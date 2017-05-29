99d92bd24dba4efbc96327c193248204f2e95fcb
Władysław Brodsky <wladyslaw.brodsky@hatimeria.pl>
2017-05-29 16:30:36 +0200

    Added logging and exception to handle order id

--- src/Controller/Adyen/Validate3d.php
+++ src/Controller/Adyen/Validate3d.php
@@ -10,11 +10,16 @@ class Validate3d extends AdyenValidate3d
      * Get order object
      *
      * @return \Magento\Sales\Model\Order
+     * @throws \Exception
      */
     protected function _getOrder()
     {
         if (!$this->_order) {
             $incrementId = $this->getRequest()->getPost('order_id');
+            if (!$incrementId) {
+                throw new \Exception('Order ID is empty');
+            }
+            $this->_adyenLogger->addAdyenResult('order id is #' . $incrementId);
             $this->_orderFactory = $this->_objectManager->get('Magento\Sales\Model\OrderFactory');
             $this->_order = $this->_orderFactory->create()->loadByIncrementId($incrementId);
         }
