2fd7ace5937d630d6706e72da6947d508a406b8d
freakphp <mail@freak.io>
2017-07-21 12:27:15 +0200

    Add X-Cache-Tags response header for url fetch rest api endpoint

--- src/Helper/Data.php
+++ src/Helper/Data.php
@@ -7,6 +7,28 @@ use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
+    const RESPONSE_TAGS_REGISTRY = 'response_tags';
+
+    /**
+     * @var \Magento\Framework\Registry
+     */
+    protected $registry;
+    /**
+     * @var \Magento\Framework\App\Cache\Tag\Resolver\Proxy
+     */
+    protected $tagResolver;
+    /**
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\App\Cache\Tag\Resolver\Proxy $registry
+     */
+    public function __construct(
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Cache\Tag\Resolver\Proxy $tagResolver
+    ) {
+        $this->registry    = $registry;
+        $this->tagResolver = $tagResolver;
+    }
+
     public function getAppLogoImg()
     {
         return $this->getConfigValue('app_logo_img');
@@ -21,4 +43,61 @@ class Data extends AbstractHelper
     {
         return $this->scopeConfig->getValue("hatimeria/reagento/$key", $scope);
     }
+
+    /**
+     * Add tags to response tag registry
+     *
+     * @param mixed $tags
+     */
+    public function addResponseTags($tags)
+    {
+        if (!is_array($tags)) {
+            $tags = [$tags];
+        }
+
+        $currentTags = $this->getResponseTags();
+        if (!is_array($currentTags)) {
+            $currentTags = [];
+        }
+
+        $tags = array_merge($currentTags, $tags);
+
+        $this->setResponseTags($tags);
+    }
+
+    /**
+     * Add response tags from object to registry
+     *
+     * @param mixed $object
+     */
+    public function addResponseTagsByObject($object)
+    {
+        $this->addResponseTags($this->tagResolver->getTags($object));
+    }
+
+    /**
+     * Set list of response tags in registry
+     * 
+     * @param mixed $tags
+     */
+    public function setResponseTags($tags)
+    {
+        if (null !== $tags && !is_array($tags)) {
+            throw new \Exception('Not accepted argument');
+        }
+
+        $this->registry->unregister(self::RESPONSE_TAGS_REGISTRY);
+        $this->registry->register(self::RESPONSE_TAGS_REGISTRY, $tags, true);
+    }
+
+    /**
+     * List of collected response tags in registry
+     *
+     * @return array
+     */
+    public function getResponseTags()
+    {
+        return $this->registry->registry(self::RESPONSE_TAGS_REGISTRY);
+    }
+
 }
