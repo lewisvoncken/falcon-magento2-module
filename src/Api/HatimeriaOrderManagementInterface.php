a7264bc78789c865b92b85c9d4dca2733423e49d
Artur Jewuła <artur.jewula@hatimeria.pl>
2017-10-12 15:13:18 +0200

    RG-1015 add endpoint for fetching logged in customer orders

--- src/Api/HatimeriaOrderManagementInterface.php
+++ src/Api/HatimeriaOrderManagementInterface.php
@@ -9,4 +9,10 @@ interface HatimeriaOrderManagementInterface
      * @return \Magento\Sales\Api\Data\OrderInterface
      */
     public function getItem($orderId);
+
+    /**
+     * @param \Magento\Framework\Api\SearchCriteria $searchCriteria
+     * @return \Magento\Sales\Api\Data\OrderSearchResultInterface
+     */
+    public function getCustomerOrders(\Magento\Framework\Api\SearchCriteria $searchCriteria);
 }
--- src/Model/OrderManagement.php
+++ src/Model/OrderManagement.php
@@ -4,9 +4,18 @@ namespace Hatimeria\Reagento\Model;
 
 use Hatimeria\Reagento\Api\HatimeriaOrderManagementInterface;
 use Magento\Authorization\Model\UserContextInterface;
+use Magento\Framework\Api\Search\FilterGroup;
+use Magento\Framework\Api\SearchCriteria;
+use Magento\Framework\Api\SortOrder;
+use Magento\Framework\Exception\InputException;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderExtensionInterface;
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderSearchResultInterface;
+use Magento\Sales\Api\Data\OrderSearchResultInterfaceFactory as SearchResultFactory;
 use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order\ShippingAssignmentBuilder;
 
 class OrderManagement implements HatimeriaOrderManagementInterface
 {
@@ -17,17 +26,35 @@ class OrderManagement implements HatimeriaOrderManagementInterface
     /** @var UserContextInterface */
     protected $userContext;
 
+    /** @var SearchResultFactory */
+    protected $searchResultFactory;
+
+    /** @var OrderExtensionFactory */
+    protected $orderExtensionFactory;
+
+    /** @var ShippingAssignmentBuilder */
+    protected $shippingAssignmentBuilder;
+
     /**
      * OrderManagement constructor.
      * @param OrderRepositoryInterface $orderRepository
      * @param UserContextInterface $userContext
+     * @param SearchResultFactory $searchResultFactory
+     * @param OrderExtensionFactory $orderExtensionFactory
+     * @param ShippingAssignmentBuilder $shippingAssignmentBuilder
      */
     public function __construct(
         OrderRepositoryInterface $orderRepository,
-        UserContextInterface $userContext
+        UserContextInterface $userContext,
+        SearchResultFactory $searchResultFactory,
+        OrderExtensionFactory $orderExtensionFactory,
+        ShippingAssignmentBuilder $shippingAssignmentBuilder
     ) {
         $this->orderRepository = $orderRepository;
         $this->userContext = $userContext;
+        $this->searchResultFactory = $searchResultFactory;
+        $this->orderExtensionFactory = $orderExtensionFactory;
+        $this->shippingAssignmentBuilder = $shippingAssignmentBuilder;
     }
 
     /**
@@ -38,10 +65,97 @@ class OrderManagement implements HatimeriaOrderManagementInterface
     public function getItem($orderId)
     {
         $order = $this->orderRepository->get($orderId);
-        if(!$order->getId() || $order->getCustomerId() !== $this->userContext->getUserId()) {
+        if(!$order->getId() || $order->getCustomerId() !== $this->getCustomerId()) {
             throw new NoSuchEntityException(__('Unable to find order %orderId', ['orderId' => $orderId]));
         }
 
         return $order;
+    }
+
+    /**
+     * Get list of customer order items
+     *
+     * @param SearchCriteria|null $searchCriteria
+     * @return OrderSearchResultInterface
+     */
+    public function getCustomerOrders(SearchCriteria $searchCriteria = null)
+    {
+        /** @var \Magento\Sales\Api\Data\OrderSearchResultInterface $searchResult */
+        $searchResult = $this->searchResultFactory->create();
+        $searchResult->addFieldToFilter('customer_id', ['eq' => $this->getCustomerId()]);
+        foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
+            $this->addFilterGroupToCollection($filterGroup, $searchResult);
+        }
+
+        $sortOrders = $searchCriteria->getSortOrders();
+        if ($sortOrders === null) {
+            $sortOrders = [];
+        }
+        /** @var \Magento\Framework\Api\SortOrder $sortOrder */
+        foreach ($sortOrders as $sortOrder) {
+            $field = $sortOrder->getField();
+            $searchResult->addOrder(
+                $field,
+                ($sortOrder->getDirection() == SortOrder::SORT_ASC) ? 'ASC' : 'DESC'
+            );
+        }
+
+        $searchResult->setSearchCriteria($searchCriteria);
+        $searchResult->setCurPage($searchCriteria->getCurrentPage());
+        $searchResult->setPageSize($searchCriteria->getPageSize());
+        foreach ($searchResult->getItems() as $order) {
+            $this->setShippingAssignments($order);
+        }
+        return $searchResult;
+    }
+
+    protected function getCustomerId()
+    {
+        return $this->userContext->getUserId();
+    }
+
+    /**
+     * Helper function that adds a FilterGroup to the collection.
+     *
+     * @param FilterGroup $filterGroup
+     * @param OrderSearchResultInterface $searchResult
+     * @return void
+     * @throws InputException
+     */
+    protected function addFilterGroupToCollection(
+        FilterGroup $filterGroup,
+        OrderSearchResultInterface $searchResult
+    ) {
+        $fields = [];
+        $conditions = [];
+        foreach ($filterGroup->getFilters() as $filter) {
+            $condition = $filter->getConditionType() ? $filter->getConditionType() : 'eq';
+            $conditions[] = [$condition => $filter->getValue()];
+            $fields[] = $filter->getField();
+        }
+        if ($fields) {
+            $searchResult->addFieldToFilter($fields, $conditions);
+        }
+    }
+
+    /**
+     * @param OrderInterface $order
+     * @return void
+     */
+    private function setShippingAssignments(OrderInterface $order)
+    {
+        /** @var OrderExtensionInterface $extensionAttributes */
+        $extensionAttributes = $order->getExtensionAttributes();
+
+        if ($extensionAttributes === null) {
+            $extensionAttributes = $this->orderExtensionFactory->create();
+        } elseif ($extensionAttributes->getShippingAssignments() !== null) {
+            return;
+        }
+        /** @var ShippingAssignmentBuilder $shippingAssignment */
+        $shippingAssignments = $this->shippingAssignmentBuilder;
+        $shippingAssignments->setOrderId($order->getEntityId());
+        $extensionAttributes->setShippingAssignments($shippingAssignments->create());
+        $order->setExtensionAttributes($extensionAttributes);
     }
 }
