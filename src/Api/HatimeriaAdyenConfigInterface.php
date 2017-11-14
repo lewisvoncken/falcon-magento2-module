f6c005627068228743ed7a45a996923eef899bc4
Artur Jewuła <artur.jewula@hatimeria.pl>
2017-11-14 19:51:28 +0100

    AR-209 add endpoint to fetch adyen config

---
+++ src/Api/HatimeriaAdyenConfigInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hatimeria\Reagento\Api;
+
+interface HatimeriaAdyenConfigInterface
+{
+    /**
+     * @return Hatimeria\Reagento\Api\Data\AdyenConfigInterface
+     */
+    public function getConfig();
+}
