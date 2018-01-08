515bbafe312db95b7e86b61b0d91f71434e9c08d
Artur Jewuła <artur.jewula@hatimeria.pl>
2018-01-08 15:22:12 +0100

    RG-1137 merge guest quote during signup

--- src/Plugin/Customer/Api/AccountManagement.php
+++ src/Plugin/Customer/Api/AccountManagement.php
@@ -4,25 +4,85 @@ namespace Hatimeria\Reagento\Plugin\Customer\Api;
 
 use Magento\Customer\Api\AccountManagementInterface;
 use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
 use Magento\Framework\Exception\InputException;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Quote\Model\Quote;
+use Psr\Log\LoggerInterface;
 
 class AccountManagement
 {
     /** @var CustomerRepositoryInterface */
     private $customerRepository;
 
+    /** @var CartRepositoryInterface */
+    private $cartRepository;
+
+    /** @var LoggerInterface */
+    private $logger;
+
     /**
      * AccountManagement constructor.
      * @param CustomerRepositoryInterface $customerRepository
+     * @param CartRepositoryInterface $cartRepository
      */
     public function __construct(
-        CustomerRepositoryInterface $customerRepository
+        CustomerRepositoryInterface $customerRepository,
+        CartRepositoryInterface $cartRepository,
+        LoggerInterface $logger
     )
     {
         $this->customerRepository = $customerRepository;
+        $this->cartRepository = $cartRepository;
+        $this->logger = $logger;
     }
 
+    /**
+     * @param AccountManagementInterface $subject
+     * @param callable $proceed
+     * @param CustomerInterface $customer
+     * @param $hash
+     * @param $redirectUrl
+     * @return CustomerInterface
+     */
+    public function aroundCreateAccountWithPasswordHash(
+        AccountManagementInterface $subject,
+        callable $proceed,
+        CustomerInterface $customer,
+        $hash,
+        $redirectUrl
+    ) {
+        $quoteId = $customer->getExtensionAttributes()->getGuestQuoteId();
+
+        /** @var CustomerInterface $result */
+        $customer = $proceed($customer, $hash, $redirectUrl);
+        if ($quoteId) {
+            try {
+                /** @var Quote $customerQuote */
+                $customerQuote = $this->cartRepository->getActiveForCustomer($customer->getId());
+                /** @var Quote $guestQuote */
+                $guestQuote = $this->cartRepository->getActive($quoteId);
+
+                $customerQuote->merge($guestQuote);
+                $this->cartRepository->save($customerQuote);
+            } catch (\Exception $e) {
+                $this->logger->critical($e);
+            }
+        }
+
+        return $customer;
+    }
+
+    /**
+     * @param AccountManagementInterface $subject
+     * @param callable $proceed
+     * @param $email
+     * @param $template
+     * @param null $websiteId
+     * @return bool
+     */
     public function aroundInitiatePasswordReset(
         AccountManagementInterface $subject,
         callable $proceed,
@@ -43,7 +103,8 @@ class AccountManagement
      * @param string $resetToken
      * @param string $newPassword
      * @return array
-     * @throws InputException
+     * @throws NoSuchEntityException
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function beforeResetPassword(AccountManagementInterface $subject, $email, $resetToken, $newPassword)
     {
