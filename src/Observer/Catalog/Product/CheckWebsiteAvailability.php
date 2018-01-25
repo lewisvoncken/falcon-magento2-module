d8afe306678d97a503ebbba51435474bd4f248f6
Artur Jewuła <artur.jewula@hatimeria.pl>
2018-01-25 15:57:15 +0100

    AR-283 check if product is available in website while adding it to cart

---
+++ src/Observer/Catalog/Product/CheckWebsiteAvailability.php
@@ -0,0 +1,34 @@
+<?php
+namespace Hatimeria\Reagento\Observer\Catalog\Product;
+
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class CheckWebsiteAvailability implements ObserverInterface
+{
+    /** @var StoreManagerInterface */
+    protected $storeManager;
+
+    /**
+     * CheckWebsiteAvailability constructor.
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(StoreManagerInterface $storeManager)
+    {
+        $this->storeManager = $storeManager;
+    }
+
+    public function execute(Observer $observer)
+    {
+        /** @var Product|ProductInterface $product */
+        $product = $observer->getEvent()->getProduct();
+
+        if (!in_array($this->storeManager->getStore()->getWebsiteId(), $product->getWebsiteIds())) {
+            $observer->getEvent()->getSalable()->setData('is_salable', 0);
+        }
+    }
+}
---
+++ src/Plugin/Quote/Model/Cart/ItemRepository.php
@@ -0,0 +1,51 @@
+<?php
+namespace Hatimeria\Reagento\Plugin\Quote\Model\Cart;
+
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
+use Magento\Catalog\Model\ResourceModel\Product\Website\Link;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\CartItemRepositoryInterface;
+use Magento\Quote\Api\Data\CartItemInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class ItemRepository
+{
+    /** @var StoreManagerInterface */
+    protected $storeManager;
+
+    /** @var ProductResource */
+    protected $productResource;
+
+    /** @var Link */
+    protected $productWebsiteLink;
+
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        ProductResource $productResource,
+        Link $productWebsiteLink
+    ) {
+        $this->storeManager = $storeManager;
+        $this->productResource = $productResource;
+        $this->productWebsiteLink = $productWebsiteLink;
+    }
+
+    /**
+     * Check if requested product is available in current store
+     *
+     * @param CartItemRepositoryInterface $subject
+     * @param CartItemInterface $cartItem
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function beforeSave(CartItemRepositoryInterface $subject, CartItemInterface $cartItem)
+    {
+        $cartItem->getSku();
+        $productId = $this->productResource->getIdBySku($cartItem->getSku());
+        $productWebsites = $this->productWebsiteLink->getWebsiteIdsByProductId($productId);
+        if (!in_array($this->storeManager->getStore()->getWebsiteId(), $productWebsites)) {
+            throw new NoSuchEntityException(__('Product that you are trying to add is not available.'));
+        }
+
+        return [$cartItem];
+    }
+}
